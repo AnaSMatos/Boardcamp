72e551e642cdeaf73631c14100fac73b1b022edf
Ana Clara Matos <anasmatos28@gmail.com>
2022-05-28 02:32:44 -0300

    feat: add categories middleware

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getCategories, postCategory } from "../controllers/categories.js";
+import validateName from "../middlewares/validateName.js";
 
 const categoriesRouter = Router();
 
 categoriesRouter.get('/categories', getCategories);
 
-categoriesRouter.post('/categories', postCategory);
+categoriesRouter.post('/categories', validateName ,postCategory);
 
 export default categoriesRouter;
